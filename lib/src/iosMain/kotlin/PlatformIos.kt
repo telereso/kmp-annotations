9fb1b6c7837f9c4173470df7998c2586fe749b45
Ahmed Alnaami <ahmedalnaami@airasia.com>
2023-07-15 12:35:54 +0800

    [CHORE] Upgrade kotlin 1.9.0

--- lib/src/iosMain/kotlin/PlatformIos.kt
+++ lib/src/iosMain/kotlin/PlatformIos.kt
@@ -1,16 +1,11 @@
+// Uncomment so project can build
+//@file:Suppress("INVISIBLE_REFERENCE", "INVISIBLE_MEMBER", "INVISIBLE_SETTER")
+
+
 package io.telereso.annotations.client
 
 import io.telereso.annotations.client.cache.AnnotationsClientDatabase
 import io.telereso.annotations.client.cache.Dao
-import platform.UIKit.UIDevice
-import io.ktor.client.*
-import io.ktor.client.engine.darwin.*
-import io.ktor.client.plugins.*
-import io.ktor.client.plugins.auth.*
-import io.ktor.client.plugins.auth.providers.*
-import io.ktor.client.plugins.contentnegotiation.*
-import io.ktor.client.plugins.logging.*
-import kotlinx.serialization.json.Json
 import com.squareup.sqldelight.db.SqlDriver
 import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
 
