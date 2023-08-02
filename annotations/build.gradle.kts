4ae03a7c84f6eaa24efe6b742a4208b14f32d60f
Ahmed na <ahmed.alnaami92@gmail.com>
2023-08-02 19:50:08 +0800

    [CHORE] Support watchos (#27)

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -11,10 +11,16 @@ version = rootProject.version
 
 kotlin {
   jvm()
+
   android()
+
   iosX64()
   iosArm64()
   iosSimulatorArm64()
+
+  watchosArm32()
+  watchosArm64()
+  watchosSimulatorArm64()
 
   js(IR) {
     browser()
@@ -31,29 +37,48 @@ kotlin {
 
     val commonMain by getting
     val commonTest by getting
+
     val androidMain by getting
     val androidTest by getting
+
     val iosX64Main by getting
     val iosArm64Main by getting
     val iosSimulatorArm64Main by getting
+
+    val watchosArm32Main by getting
+    val watchosArm64Main by getting
+
     val iosMain by creating {
       dependsOn(commonMain)
       iosX64Main.dependsOn(this)
       iosArm64Main.dependsOn(this)
       iosSimulatorArm64Main.dependsOn(this)
+
+      watchosArm32Main.dependsOn(this)
+      watchosArm64Main.dependsOn(this)
     }
     val iosX64Test by getting
     val iosArm64Test by getting
     val iosSimulatorArm64Test by getting
+
+    val watchosArm32Test by getting
+    val watchosArm64Test by getting
+    val watchosSimulatorArm64Test by getting
+
     val iosTest by creating {
       dependsOn(commonTest)
       iosX64Test.dependsOn(this)
       iosArm64Test.dependsOn(this)
       iosSimulatorArm64Test.dependsOn(this)
+
+      watchosArm32Test.dependsOn(this)
+      watchosArm64Test.dependsOn(this)
+      watchosSimulatorArm64Test.dependsOn(this)
     }
 
     val jvmMain by getting
     val jvmTest by getting
+
     val jsMain by getting
     val jsTest by getting
   }
