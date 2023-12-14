da751b29f40cfff6877130cb2ce42556bfb8cd12
Ahmed Alnaami <ahmedalnaami@airasia.com>
2023-12-14 18:34:39 +0800

    [FEAT] Support Native Target

--- annotations/build.gradle.kts
+++ annotations/build.gradle.kts
@@ -28,6 +28,16 @@ kotlin {
     binaries.executable()
   }
 
+  val hostOs = System.getProperty("os.name")
+  val isMingwX64 = hostOs.startsWith("Windows")
+  when {
+    hostOs == "Mac OS X" -> macosX64("native")
+    hostOs == "Linux" -> linuxX64("native")
+    isMingwX64 -> mingwX64("native")
+    else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
+  }
+
+
   sourceSets {
 
     all {
@@ -80,6 +90,9 @@ kotlin {
 
     val jsMain by getting
     val jsTest by getting
+
+    val nativeMain by getting
+    val nativeTest by getting
   }
 }
 
